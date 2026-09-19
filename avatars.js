3253924edfa2f61f8374debeba78a48f8e960280
wjb000 <127271713+wjb000@users.noreply.github.com>
2026-09-18 20:13:32 -0700

    Swap in official VRoid anime sample avatars A/B/C

---
+++ avatars.js
@@ -0,0 +1,9 @@
+const VRM = "https://cdn.jsdelivr.net/gh/madjin/vrm-samples@master/vroid/stable/";
+const GLB = "https://cdn.jsdelivr.net/gh/mrdoob/three.js@r160/examples/models/gltf/";
+
+export const CAST = [
+  { id: "aya", name: "Aya", url: VRM + "AvatarSample_A.vrm", scale: 1, vrm: true },
+  { id: "haru", name: "Haru", url: VRM + "AvatarSample_B.vrm", scale: 1, vrm: true },
+  { id: "ren", name: "Ren", url: VRM + "AvatarSample_C.vrm", scale: 1, vrm: true },
+  { id: "robot", name: "Robot", url: GLB + "RobotExpressive/RobotExpressive.glb", scale: 1 },
+];
