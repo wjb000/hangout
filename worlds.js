259a32eae79670ede8f4ce6b795e6fb09d5d4cb4
wjb000 <127271713+wjb000@users.noreply.github.com>
2026-09-18 19:52:33 -0700

    Drop Littlest Tokyo; only CryEngine Sponza + Babylon house level

---
+++ worlds.js
@@ -0,0 +1,18 @@
+export const WORLDS = [
+  {
+    id: "sponza",
+    name: "Sponza",
+    engine: "CryEngine",
+    url: "https://cdn.jsdelivr.net/gh/KhronosGroup/glTF-Sample-Assets@main/Models/Sponza/glTF/Sponza.gltf",
+    spawn: { x: 0, z: 0 },
+    height: 12,
+  },
+  {
+    id: "house",
+    name: "House",
+    engine: "Babylon",
+    url: "https://assets.babylonjs.com/meshes/house_scene.glb",
+    spawn: { x: 0, z: 2 },
+    height: 6,
+  },
+];
